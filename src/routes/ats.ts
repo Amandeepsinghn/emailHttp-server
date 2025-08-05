3dfc08d384d7da5294858ef5763bd4df152cd030
Amandeepsinghn <amandeepsingh.kaillay@gmail.com>
2025-08-05 19:03:48 +0530

    feat backend is done

--- src/routes/ats.ts
+++ src/routes/ats.ts
@@ -1,25 +1,23 @@
-import express,{ Router } from "express";
+import express, { Router } from "express";
 import { middleware } from "../middleware";
-import { getAllData, scanResume } from "../controllers/atsController";
+import { getAllData, getSingleResume, scanResume } from "../controllers/atsController";
 import multer from "multer";
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null,'uploads/'); 
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
-  }
+  },
 });
 
-const upload = multer({storage });
+const upload = multer({ storage });
 
+router.get("/getAllResume", middleware, getAllData);
+router.post("/upload-pdf", middleware, upload.single("file"), scanResume);
+router.get("/item/:itemId", middleware, getSingleResume);
 
-router.get("/getAllResume",middleware,getAllData)
-router.post("/upload-pdf",middleware,upload.single('file'),scanResume)
-router.get("/item/:itemId",middleware,)
-
-
-export const atsRouter:Router = router
+export const atsRouter: Router = router;
